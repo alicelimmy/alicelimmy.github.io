2fd2c94a151f6d0f120434e633d73d34eb46d846
Alice Lim <alicelim@Alices-MBP.wireless.newschool.edu>
2015-05-09 12:50:49 -0400

    mesaf

---
+++ Main Projects/final/finalhome.js
@@ -0,0 +1,66 @@
+$(function (){
+	$("#navigation").click(function(){
+		
+		$("#hamburger").toggleClass ("show");
+	});
+	
+	
+});
+
+$(function (){
+	$("#search").click(function(){
+		
+		$("#searchbar").toggleClass ("show");
+	});
+	
+	
+});
+
+$(function (){
+	$("#search2").click(function(){
+		
+		$("#searchbar2").toggleClass ("show");
+	});
+	
+	
+});
+
+
+//__________________ajax_____________________
+
+$(
+	function () {
+		function requestFile(url){
+		$.ajax(
+			{
+				url: url,
+				cache: false,
+				dataType: "json",
+				complete: function(data){
+				
+					$("#result").html("Thank You!");	
+				
+					
+				}
+				
+		
+			}
+		);
+		
+		}
+	
+		
+		$(".submit").click(function(){
+			if($("#newsletterform").val() == "") {
+				$("#result").html("Oh Noez!");	
+				return;
+			}
+		 	requestFile("data.json");
+	}
+	 )
+								
+								 
+								 					 
+								 
+	});
+
