807fceb3b54696e33392b2debc93be8ed613803f
Alice Lim <alicelim@Alices-MacBook-Pro.local>
2015-05-09 20:30:08 -0400

    dsaf

--- Main Projects/final/finalhome.js
+++ Main Projects/final/finalhome.js
@@ -1,67 +1,52 @@
 $(function (){
 	$("#navigation").click(function(){
-		
 		$("#hamburger").toggleClass ("show");
 	});
-	
-	
 });
 
 $(function (){
 	$("#search").click(function(){
-		
 		$("#searchbar").toggleClass ("show");
 	});
-	
-	
 });
 
 $(function (){
 	$("#search2").click(function(){
-		
 		$("#searchbar2").toggleClass ("show");
 	});
-	
-	
 });
 
 
 //__________________ajax_____________________
 
-$(
-	function () {
-		function requestFile(url){
+$(function () {
+	function requestFile(url){
 		$.ajax(
-			{
-				url: url,
-				cache: false,
-				dataType: "json",
-				complete: function(data){
-				
-					$("#result").html("THANK YOU!");	
-				
-					
+			{url: url,
+			cache: false,
+			dataType: "json",
+			success: function(data){
+				$("#result").html("THANK YOU!");
 				}
-				
-		
 			}
 		);
-		
-		}
-	
+	}
+
 		
 		$(".submit").click(function(){
 			if($("#newsletterform").val() == "") {
 				$("#result").html("Uh Oh, Please Fill In a Full Email");	
 				return;
+				
 				$("#result").css({"color":"red"});
 			}
+			
 		 	requestFile("data.json");
-	}
-	 )
-								
-								 
-								 					 
-								 
-	});
+		})						 
+});
 
+
+
+//var temp = $(".searchbox").val();
+//url = "http://google.com/" + temp;
+//window.location(url);
